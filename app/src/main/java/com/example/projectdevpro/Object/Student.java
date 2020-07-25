c609bcdc980012e6744e72b7988d1cd988a3bd06
VuXuanThe <thevu2907@gmail.c>
2020-07-25 12:00:40 +0700

    add Fragment Information Student

---
+++ app/src/main/java/com/example/projectdevpro/Object/Student.java
@@ -0,0 +1,26 @@
+package com.example.projectdevpro.Object;
+
+import android.graphics.Bitmap;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private int     idStudent;
+    private String  linkAvatarSt;
+    private Bitmap avatar;
+    private String  Name;
+    private String  password;
+    private String  experient;
+    private String  job;
+    private String  subject;
+    private String  gender;
+    private int     age;
+    private String  vehicle;
+    private String  freeTime;
+    private int     tuition;
+    private String  email;
+    private String  numberPhone;
+    private String  processWork;
+    private double  longitude;
+    private double  latitude;
+}
